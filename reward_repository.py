8f2ecf0dad13c9bf0744685902eac72154f03e77
xro0g9s0 <rosatolen@gmail.com>
2014-10-23 00:27:20 -0700

    adding ability to add and see rewards through a browser

--- reward_repository.py
+++ reward_repository.py
@@ -1,17 +1,28 @@
 import sys
 from pymongo import MongoClient
+from reward import *
 
 class RewardRepository:
   def __init__(self):
     self.rewards = MongoClient('mongodb://localhost:27017').velocity.rewards
 
+  def add(self, reward):
+    self.rewards.insert({ "name": reward.name, "cost": reward.cost })
+
+  def get_rewards(self):
+    rewards = []
+    for reward in self.rewards.find():
+      rewards.append(Reward(reward['name'], reward['cost']))
+    return rewards
+
   def insert_reward(self, name, cost):
-    new_reward = { "Name" : name, "Cost" : cost }
+    new_reward = { "name" : name, "Cost" : cost }
     self.rewards.insert(new_reward)
 
   def view_all_rewards(self):
     for reward in self.rewards.find():
       print(reward)
+      print(reward['name'])
 
 def main(arguments):
   repository = RewardRepository()
--- view.py
+++ view.py
@@ -1,15 +1,39 @@
 import web
 from web.contrib.template import render_jinja
+from reward_repository import *
+from reward import *
 
-urls = (
-'/(.*)', 'Home'
-)
-render = render_jinja('templates', encoding='utf-8',)
-app = web.application(urls, globals())
+def main():
+  urls = (
+    '/', 'Home'
+  )
+  app = web.application(urls, globals())
+  app.run()
+
+render = web.template.render('templates')
 
 class Home:
-  def GET(self, name):
-    return render.hello(name=name)
+  form = web.form.Form(
+    web.form.Textbox('Name',  web.form.notnull),
+    web.form.Textbox('Cost', web.form.notnull),
+    web.form.Button('Add Reward'),
+  )
+
+  def GET(self):
+    reward_repository = RewardRepository()
+    rewards = reward_repository.get_rewards()
+    form = self.form()
+    return render.home(rewards, form)
+
+  def POST(self):
+    form = self.form()
+    if not form.validates():
+      return render.home(form)
+
+    reward = Reward(form.d.Name, form.d.Cost)
+    reward_repository = RewardRepository()
+    reward_repository.add(reward)
+    raise web.seeother('/')
 
 if __name__ == "__main__":
-  app.run()
+  main()
