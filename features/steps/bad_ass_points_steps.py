fc20817e0f2c88256d5854696e9f14932e8451ce
xro0g9s0 <rosatolen@gmail.com>
2014-10-30 00:08:22 -0700

    refactoring points purse steps

--- features/steps/bad_ass_points_steps.py
+++ features/steps/bad_ass_points_steps.py
@@ -1,20 +1,16 @@
-from behave import *
+from behave import then, use_step_matcher
 from nose import tools
 from model.bad_ass_points_purse import BadAssPointsPurse
 from model.repositories.bad_ass_points_repository import BadAssPointsRepository
 from model.repositories.mongo_wrapper import MongoWrapper
+use_step_matcher("parse")
 
-@then(u'I should have 1 more bad ass point in my bad ass points purse')
-def step_impl(context):
+@then(u'I should have {amount} more bad ass points in my bad ass points purse')
+def step_impl(context, amount):
     bad_ass_points_purse = BadAssPointsPurse(BadAssPointsRepository(MongoWrapper()))
-    tools.assert_equal(1, bad_ass_points_purse.total)
+    tools.assert_equal(int(amount), bad_ass_points_purse.total)
 
-@then(u'I should have 10 more bad ass points in my bad ass points purse')
-def step_impl(context):
-    bad_ass_points_purse = BadAssPointsPurse(BadAssPointsRepository(MongoWrapper()))
-    tools.assert_equal(11, bad_ass_points_purse.total)
-
-@then(u'I should see my bad ass points increase to 10')
-def step_impl(context):
+@then(u'I should see my bad ass points increase to {amount}')
+def step_impl(context, amount):
     bad_ass_points_total = context.home_page.get_bad_ass_points_total()
-    tools.assert_equal(10, bad_ass_points_total)
+    tools.assert_equal(int(amount), bad_ass_points_total)
