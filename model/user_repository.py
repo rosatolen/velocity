4cefde6f2f8049ced703692de856549d96605303
xro0g9s0 <rosatolen@gmail.com>
2014-11-10 22:45:02 -0800

    refactoring save_state into smaller functions

--- model/user_repository.py
+++ model/user_repository.py
@@ -127,28 +127,10 @@ class UserRepository:
         password = db_user['password']
         salt = db_user['salt']
 
-        changed_total = find_changed(db_user['purse']['total'], user.purse.total)
-        changed_todays_total = find_changed(db_user['purse']['todays_total'], user.purse.todays_total)
-        changed_updated_day = find_changed(db_user['purse']['last_updated_date'],
-                                           date_at_midnight(user.purse.last_updated_day))
-
-        current_rewards = self.get_rewards(user.username)
-        changed_rewards = find_changed(current_rewards, user.rewards)
-        dict_rewards = []
-        for reward in changed_rewards:
-            dict_rewards.append(dict(zip(('name', 'cost'), (reward.name, reward.cost))))
-
-        current_tasks = self.get_tasks(user.username)
-        changed_tasks = find_changed(current_tasks, user.tasks)
-        dict_tasks = []
-        for task in changed_tasks:
-            dict_tasks.append(dict(zip(('name', 'size'), (task.name, size(task)))))
-
-        current_habits = self.get_habits(user.username)
-        changed_habits = find_changed(current_habits, user.habits)
-        dict_habits = []
-        for habit in changed_habits:
-            dict_habits.append(dict([('name', habit.name)]))
+        changed_todays_total, changed_total, changed_updated_day = self.get_changed_purse_data(db_user, user)
+        dict_rewards = self.create_rewards_dict(user)
+        dict_tasks = self.create_tasks_dict(user)
+        dict_habits = self.create_habits_dict(user)
 
         self.storage_connection.remove(user_query)
         self.storage_connection.insert({
@@ -164,6 +146,37 @@ class UserRepository:
             'tasks': dict_tasks,
             'habits': dict_habits
         })
+
+    def create_habits_dict(self, user):
+        current_habits = self.get_habits(user.username)
+        changed_habits = find_changed(current_habits, user.habits)
+        dict_habits = []
+        for habit in changed_habits:
+            dict_habits.append(dict([('name', habit.name)]))
+        return dict_habits
+
+    def create_tasks_dict(self, user):
+        current_tasks = self.get_tasks(user.username)
+        changed_tasks = find_changed(current_tasks, user.tasks)
+        dict_tasks = []
+        for task in changed_tasks:
+            dict_tasks.append(dict(zip(('name', 'size'), (task.name, size(task)))))
+        return dict_tasks
+
+    def create_rewards_dict(self, user):
+        current_rewards = self.get_rewards(user.username)
+        changed_rewards = find_changed(current_rewards, user.rewards)
+        dict_rewards = []
+        for reward in changed_rewards:
+            dict_rewards.append(dict(zip(('name', 'cost'), (reward.name, reward.cost))))
+        return dict_rewards
+
+    def get_changed_purse_data(self, db_user, user):
+        changed_total = find_changed(db_user['purse']['total'], user.purse.total)
+        changed_todays_total = find_changed(db_user['purse']['todays_total'], user.purse.todays_total)
+        changed_updated_day = find_changed(db_user['purse']['last_updated_date'],
+                                           date_at_midnight(user.purse.last_updated_day))
+        return changed_todays_total, changed_total, changed_updated_day
 
 
 def date_at_midnight(day):
